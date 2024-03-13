7ca23e00881692c0e54c93a482eb5576dffeb141
Owen Menzies <78829256+OwenMenzies@users.noreply.github.com>
2024-03-13 13:37:08 +1300

    started history
    added logging the questions to a statement

--- openai-test2.js
+++ openai-test2.js
@@ -2,14 +2,15 @@ import OpenAI from "openai";
 import readline from 'readline';
 
 const openai = new OpenAI();
-
+var i = 0;
+const history = new Array()
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 async function callgpt(question) {
-  console.log('point 2');
+  
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: "Greetings" }, { role: "user", content: question }],
     model: "gpt-3.5-turbo",
@@ -24,14 +25,17 @@ console.log('Hello, this is my module!');
 async function askQuestion() {
   const userInput = await new Promise(resolve => {
     rl.question('What would you like to ask the all mighty? (Enter "0" to exit): ', resolve);
+   
   });
 
   if (userInput !== '0') {
     console.log(`You asked: ${userInput}\nNow await your response from the divine`);
-
+    history[i] = userInput;
+    i = i+1 
+    console.log(history)
     // Call the OpenAI API after getting user input
     const answer = await callgpt(userInput);
-    console.log('point 1');
+    
     console.log(answer);
 
     // Ask the next question
